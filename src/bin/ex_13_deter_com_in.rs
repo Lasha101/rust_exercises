703efa14cacd1714cac72e32fc1e75b3db8c54e9
Lasha101 <gurshabo55@gmail.com>
2026-04-14 03:20:49 +0400

    feat: add compound interest CLI and fix simple interest math
    Adds the new compound interest calculator with standard I/O prompting. Additionally, updates the simple interest calculation to correctly divide the percentage input by 100.

--- src/bin/ex_13_deter_com_in.rs
+++ src/bin/ex_13_deter_com_in.rs
@@ -5,19 +5,24 @@ fn main() {
     let display_strings: &[&str] = 
     &["Enter the principal: ", 
     "Enter the rate of interest: ", 
-    "Enter the number of years: "];
+    "Enter the number of years: ",
+    "What is the number of times the interest is compounded per year? "];
 
     let inputed_values = collect_inputs(
                               display_strings);
 
-    let final_amount: f64 = calc_simple_interest(
+    let final_amount: f64 = calc_compound_interest(
                                         &inputed_values[0],
                                         &inputed_values[1],
-                                        &inputed_values[2]);
+                                        &inputed_values[2],
+                                        &inputed_values[3]
+                                    );
 
     print_final_string(inputed_values[0], 
                        inputed_values[1],
-                        final_amount);
+                       inputed_values[2],
+                       inputed_values[3],
+                       final_amount);
 }
 
 fn get_user_input(prompt_text: &str) -> String {
@@ -28,8 +33,8 @@ fn get_user_input(prompt_text: &str) -> String {
     text.trim().to_string() 
 }
 
-fn collect_inputs(arr_str: &[&str]) -> [f64; 3] {
-    let mut user_inputs = [0.0, 0.0, 0.0];
+fn collect_inputs(arr_str: &[&str]) -> [f64; 4] {
+    let mut user_inputs = [0.0, 0.0, 0.0, 0.0];
     for (index, txt) in arr_str.iter().enumerate() {
         loop {
             let input = get_user_input(*txt);
@@ -45,17 +50,20 @@ fn collect_inputs(arr_str: &[&str]) -> [f64; 3] {
     user_inputs
 }
 
-fn calc_simple_interest(principal: &f64, interest: &f64, years: &f64) -> f64 {
-    principal * (1.0 + (interest/100.0) * years)
+fn calc_compound_interest(principal: &f64,
+                        interest: &f64, 
+                        years: &f64, 
+                        per_year: &f64) -> f64 {
+    principal * (1.0 + (interest/100.0)/per_year).powf(per_year*years)
 }
 
 
 
 
-fn print_final_string(years: f64, rate: f64, final_amount: f64) {
+fn print_final_string(principal: f64, rate: f64, years: f64, per_year: f64, final_amount: f64) {
     let years_as_int = years as i32;
-    let rounded_final_amount = final_amount.round();
-    println!("After {} years at {}%, the investment will\n\
-              be worth ${}.",
-              years_as_int, rate, rounded_final_amount);
+    let rounded_final_amount = (final_amount * 100.0).round()/100.0;
+    println!("${} invested at {}% for {} years\n\
+              compounded {} times per year is ${}.",
+              principal, rate, years_as_int, per_year, rounded_final_amount);
 }
