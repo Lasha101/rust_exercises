8dac88b05f2f81a764e2746780681946e6ae00e7
Lasha101 <gurshabo55@gmail.com>
2026-04-26 15:59:58 +0400

    Refactor: eliminate all variables to meet exercise challenge requirements

--- src/bin/ex_1_saying_hello.rs
+++ src/bin/ex_1_saying_hello.rs
@@ -2,9 +2,21 @@ use std::io::{self, Write};
 
 fn main() {
     print!("What is your name? ");
-    io::stdout().flush().expect("Error");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("Fail to read");
-    let greeting = format!("Hello, {}, nice to meet you!", name.trim());
-    println!("{}", greeting);
+    io::stdout().flush().expect("Fail to write.");
+    for line in io::stdin().lines() {
+        println!("{}", format!("Hello, {}, nice to meet you!", line.expect("Fail to read.").trim()));
+        break;
+    }   
 }
+
+// fn main() {
+//     print!("What is your name? ");
+//     io::stdout().flush().expect("Error");
+//     let mut name = String::new();
+//     io::stdin().read_line(&mut name).expect("Fail to read");
+//     let greeting = format!("Hello, {}, nice to meet you!", name.trim());
+//     println!("{}", greeting);
+// }
+
+
+
