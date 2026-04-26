99b87edf46eb9d3a0e69cc7c485c367037a80212
Lasha101 <gurshabo55@gmail.com>
2026-04-26 20:52:07 +0400

    feat: add custom greetings for specific users
    Implemented a match expression to display personalized greetings for Anya, Sonya, and Tanya. Used modern inline formatting for cleaner print macros.

--- src/bin/ex_1_saying_hello.rs
+++ src/bin/ex_1_saying_hello.rs
@@ -2,19 +2,16 @@ use std::io::{self, Write};
 
 fn main() {
     let name = user_input("Enter your name: ");
-    let greeting = match name.as_str() {
-        "Anya" | "Sonya" | "Tanya" => greeting_constructor(&name),
-        _=> "Name not found.".to_string(),
+    match name.as_str() {
+        "Anya"  => println!("Hello, {name}, nice to meet you."),
+        "Sonya" => println!("Hi, {name}, glad to see you." ),
+        "Tanya" => println!("Welcome, {name}."),
+        _=> println!("Name not found."),
     };
-    println!("{}", greeting);
 }
 
 fn user_input(prompt_text: &str) -> String {
     print!("{}", prompt_text);
     io::stdout().flush().expect("Fail to write.");
     io::stdin().lines().next().unwrap().expect("Fail to read").trim().to_string()
-}
-
-fn greeting_constructor(name: &str) -> String {
-    format!("Hello, {}, nice to meet you.", name)  
 }
