163b3c517f389f5192e141f76f7450f3ceefde78
Lasha101 <gurshabo55@gmail.com>
2026-04-26 20:30:34 +0400

    refactor: optimize standard input reading
    Replaced previous logic with a highly idiomatic, memory-safe, and performant implementation using string slices and pattern matching.

--- src/bin/ex_1_saying_hello.rs
+++ src/bin/ex_1_saying_hello.rs
@@ -1,13 +1,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("What is your name? ");
-    io::stdout().flush().expect("Fail to write.");
-    println!(
-        "Hello, {}, nice to meet you!", 
-        io::stdin().lines().next().unwrap().expect("Fail to read").trim()
-    );
+    let name = user_input("Enter your name: ");
+    let greeting = match name.as_str() {
+        "Anya" | "Sonya" | "Tanya" => greeting_constructor(&name),
+        _=> "Name not found.".to_string(),
+    };
+    println!("{}", greeting);
 }
 
+fn user_input(prompt_text: &str) -> String {
+    print!("{}", prompt_text);
+    io::stdout().flush().expect("Fail to write.");
+    io::stdin().lines().next().unwrap().expect("Fail to read").trim().to_string()
+}
 
-
+fn greeting_constructor(name: &str) -> String {
+    format!("Hello, {}, nice to meet you.", name)  
+}
