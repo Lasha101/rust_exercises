4bc6aee991b8f07bf759587fd9f03f7b5a2a9a83
Lasha101 <gurshabo55@gmail.com>
2026-05-06 15:38:09 +0400

    Handle no-difference and negative cases

--- terminal_exercises/src/bin/ex_6_retirement_calc.rs
+++ terminal_exercises/src/bin/ex_6_retirement_calc.rs
@@ -13,10 +13,16 @@ fn main() {
         inputed_values[1], 
         dt, 
     );
-
-    print_final_string(&left_ret_years.0,
+    
+    if left_ret_years.0 > 0 {
+        print_final_string(&left_ret_years.0,
                        &left_ret_years.1,
                        dt);
+    } else if left_ret_years.0 == 0 {
+        println!("You can retire in this year.")
+    } else {
+        println!("You can already retire.")
+    } 
 }
 
 fn get_user_input(prompt_text: &str) -> String {
@@ -54,7 +60,8 @@ fn calculate_left_ret_year(age: i32, want_to_retire: i32, current_year: i32) -> 
 
 fn print_final_string(left_years: &i32, ret_year: &i32, current_year: i32) {
     println!(
-"You have {} years left until you can retire.\nIt's {}, so you can retire in {}.",
+            "You have {} years left until you can retire.\n\
+            It's {}, so you can retire in {}.",
               left_years, current_year, ret_year
             );   
 }
