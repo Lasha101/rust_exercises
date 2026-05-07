887bb889e349e32016e18d3c7f6ca1f6423a1cc8
Lasha101 <gurshabo55@gmail.com>
2026-05-08 02:34:25 +0400

    Add an implementation for a round room.

--- terminal_exercises/src/bin/ex_9_paint_calculator.rs
+++ terminal_exercises/src/bin/ex_9_paint_calculator.rs
@@ -2,60 +2,47 @@ use std::io::{self, Write};
 
 
 fn main() {
-    let display_strings: &[&str] = 
-    &["What is the length of the room in feet? ", 
-    "What is the width of the room in feet? "];
+    let display_string: &str = &"What is the radius of the room in feet? ";
 
-    let inputed_values = collect_inputs(
-                              display_strings);
+    let radius = get_user_input(display_string);
 
-    let total_area = area(inputed_values[0],
-                         inputed_values[1]);
+    let area = area(&radius);
 
     const INDEX: i32 = 350;
 
-    let num_of_gallons = num_of_gallons_to_paint(&total_area, &INDEX);
+    let num_of_gallons = num_of_gallons_to_paint(&area, &INDEX);
 
-    print_final_string(num_of_gallons, total_area);
+    print_final_string(num_of_gallons, area);
 }
 
-fn get_user_input(prompt_text: &str) -> String {
-    print!("{}", prompt_text);
-    io::stdout().flush().expect("Error to show the text!");
-    let mut text = String::new();
-    io::stdin().read_line(&mut text).expect("Fail to read text!");
-    text.trim().to_string() 
-}
-
-fn collect_inputs(arr_str: &[&str]) -> Vec<i32> {
-    let mut user_inputs = Vec::new();
-    for txt in arr_str {
-        loop {
-            let input = get_user_input(*txt);
-            match  input.trim().parse::<i32>() {
-                Ok(parsed_number) => {
-                    user_inputs.push(parsed_number);
-                    break;
-                },
-                Err(error_message) => println!("{}", error_message),
+fn get_user_input(prompt_text: &str) -> f64 {
+    loop {
+        print!("{}", prompt_text);
+        io::stdout().flush().expect("Error to show the text!");
+        let mut text = String::new();
+        io::stdin().read_line(&mut text).expect("Fail to read text!");
+        match  text.trim().parse::<f64>() {
+            Ok(parsed_number) => break parsed_number,
+            Err(error_message) => {
+                println!("{}", error_message);
+                continue;
             }
         }
-    }
-    user_inputs
+    }   
 }
 
-fn area(length: i32, width: i32) -> i32 {
-    length * width 
+fn area(radius: &f64) -> f64 {
+    std::f64::consts::PI * radius.powf(2.0)  
 }
 
-fn num_of_gallons_to_paint(area: &i32, index: &i32) -> i32 {
-    let result = (*area as f64) / (*index as f64);
+fn num_of_gallons_to_paint(area: &f64, index: &i32) -> i32 {
+    let result = *area / (*index as f64);
     result.ceil() as i32
 }
 
 
-fn print_final_string(num_of_gallons: i32, area: i32) {
+fn print_final_string(num_of_gallons: i32, area: f64) {
     println!("You will need to purchase {} gallons of\n\
-              paint to cover {} square feet.",
-             num_of_gallons, area);
+              paint to cover {:.2} square feet.",
+              num_of_gallons, area);
 }
