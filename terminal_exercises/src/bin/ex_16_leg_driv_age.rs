ac7ea70e18557ac5011be9875fec30585da1993e
Lasha101 <gurshabo55@gmail.com>
2026-06-07 23:49:17 +0400

    Implement features to resolve chalenge constraints.

--- terminal_exercises/src/bin/ex_16_leg_driv_age.rs
+++ terminal_exercises/src/bin/ex_16_leg_driv_age.rs
@@ -1,11 +1,20 @@
 use std::io::{self, Write};
+use std::collections::HashMap;
 
 fn main() {
+    let mut driving_age_by_country = HashMap::new();
+    driving_age_by_country.insert("El Salvador".to_string(), 15);
+    driving_age_by_country.insert("United Kingdom".to_string(), 17);
+    driving_age_by_country.insert("Japan".to_string(), 18);
+
     let display_string: &str = "What is your age? ";
 
     let inputed_value = validate_input(display_string);
-    if comparing_function(inputed_value) {
-        println!("You are old enough to legally drive.");
+
+    let countries = comparing_function(inputed_value, driving_age_by_country);
+    if !countries.is_empty() {
+        // Joins the country names with a comma and a space
+        println!("You are old enough to legally drive in {}.", countries.join(", "));
     } else {
         println!("You are not old enough to legally drive.");
     }
@@ -23,13 +32,19 @@ fn validate_input(txt: &str) -> i32 {
     loop {
         let input = get_user_input(txt);
         match  input.trim().parse::<i32>() {
-            Ok(parsed_number) => return parsed_number,
+            Ok(parsed_number) if parsed_number >= 1 => return parsed_number,
+            Ok(_) => println!("Invalid input. The number must be greater than 0."),
             Err(_) => println!("Invalid input. Please enter a whole number."),
         }
     }
 }
 
-fn comparing_function(age: i32) -> bool {
-    age >= 16
+fn comparing_function(input_age: i32, driving_age_country: HashMap<String, i32>) -> Vec<String> {
+    let countries: Vec<String> = driving_age_country
+        .into_iter()
+        .filter(|&(_, age)| input_age >= age)
+        .map(|(name, _)| name)
+        .collect();
+    countries
 }
 
