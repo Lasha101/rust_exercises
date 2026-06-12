4c99017b34c0f6c6dd832636f1d7e54be5f1284e
Lasha101 <gurshabo55@gmail.com>
2026-06-13 03:17:49 +0400

    Implement challeng's prompt for ex_26.

--- terminal_exercises/src/bin/ex_26_credit_card.rs
+++ terminal_exercises/src/bin/ex_26_credit_card.rs
@@ -1,20 +1,40 @@
 use std::io::{self, Write};
 
 fn main() {
-    let display_strings: &[&str] = 
-    &[
-    "What is your balance? ", 
-    "What is the APR on the card (as a percent)? ",
-    "What is the monthly payment you can make? "
+    let display_strings: &[&str] = &[
+        "What is your balance? ", 
+        "What is the APR on the card (as a percent)? ",
+        "Choice: for calculate months(0) or monthly payment(1): "
     ];
 
-    let inputed_values = collect_inputs(
-                              display_strings);
-
-    let num_of_months: f64 = calculator(&inputed_values).ceil();
-
-    println!("It will take you {} months to pay off this card.", num_of_months)
+    let inputed_values = collect_inputs(display_strings);
     
+    let result: f64;
+
+    if inputed_values[2] == 0.0 {
+        let monthly_payment = loop {
+            let additional_input = get_user_input("What is the monthly payment you can make? ");
+            match validate_nums(&additional_input) {
+                Ok(num) => break num,
+                Err(e) => println!("{}", e),
+            }
+        };
+        let calc_values = [inputed_values[0], inputed_values[1], monthly_payment];
+        result = monts_calculator(&calc_values).ceil();
+        println!("It will take you {} months to pay off this card.", result);
+    } else {
+        let months = loop {
+            let additional_input = get_user_input("What is the number of months to payoff? ");
+            match validate_nums(&additional_input) {
+                Ok(num) => break num,
+                Err(e) => println!("{}", e),
+            }
+        };
+        
+        let calc_values = [inputed_values[0], inputed_values[1], months];
+        result = monthly_payment_calculator(&calc_values);
+        println!("Amount of monthly payment is {:.2}", result);
+    }  
 }
 
 fn get_user_input(prompt_text: &str) -> String {
@@ -25,19 +45,18 @@ fn get_user_input(prompt_text: &str) -> String {
     text.trim().to_string() 
 }
 
-fn validate_nums(input:&str) -> Result<f64, String> {
+fn validate_nums(input: &str) -> Result<f64, String> {
     match input.trim().parse::<f64>() {
         Ok(number) => Ok(number),
-        Err(_) => Err(String::from("Cannot calculate value.")),
+        Err(_) => Err(String::from("Cannot calculate value. Please enter a valid number.")),
     }
 }
 
 fn collect_inputs(arr_str: &[&str]) -> [f64; 3] {
-    let mut user_inputs = [0.0, 0.0, 0.0];
+    let mut user_inputs = [0.0, 0.0, 0.0]; 
     for (index, txt) in arr_str.iter().enumerate() {
         loop {
             let input = get_user_input(txt);
-            
             let result = validate_nums(&input);
             match result {
                 Ok(number) => {
@@ -51,10 +70,20 @@ fn collect_inputs(arr_str: &[&str]) -> [f64; 3] {
     user_inputs
 }
 
-fn calculator(values: &[f64; 3]) -> f64 {
+fn monts_calculator(values: &[f64; 3]) -> f64 {
     let mltp = -1.0 / 30.0;
     let i = (values[1] / 100.0) / 365.0;
     let numerator = (1.0 + (values[0] / values[2]) * (1.0 - (1.0 + i).powf(30.0))).ln();
     let denominator = (1.0 + i).ln();
     mltp * numerator / denominator
+}
+
+fn monthly_payment_calculator(values: &[f64; 3]) -> f64 {
+    let i = (values[1] / 100.0) / 365.0;
+
+    let time_factor = (1.0 + i).powf(30.0 * values[2]);
+    let numerator = values[0] * i * time_factor;
+    let denominator = time_factor - 1.0;
+    
+    (numerator / denominator) * 30.0 
 }
